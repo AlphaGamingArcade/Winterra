23af6bb4c05067468686e7c8e1666cbf201029d3
AlphaGamingArcade <155131996+AlphaGamingArcade@users.noreply.github.com>
2025-04-16 10:36:19 +0800

    feat

--- Models/ViewModels/HomeViewModel.cs
+++ Models/ViewModels/HomeViewModel.cs
@@ -2,8 +2,20 @@
 
 namespace Winterra.Models.ViewModels
 {
-    public class HomeViewModel : BaseViewModel
-    {
+  public class HomeViewModel : BaseViewModel
+  {
+    public IEnumerable<Account>? UserAccountList { get; set; }
+    public IEnumerable<Account>? AdminAccountList { get; set; }
+    public IEnumerable<Preview>? CharacterPreviewList { get; set; }
+    public IEnumerable<Preview>? HighlightPreviewList { get; set; }
+    public IEnumerable<Preview>? LorePreviewList { get; set; }
+    public IEnumerable<Content>? FeaturesContentList { get; set; }
+    public IEnumerable<Content>? NewsContentList { get; set; }
+    public IEnumerable<Content>? UpdateContentList { get; set; }
+    public IEnumerable<Content>? CodeOfConductContentList { get; set; }
+    public IEnumerable<Content>? TermsOfUseContentList { get; set; }
+    public IEnumerable<Content>? PrivacyPolicyContentList { get; set; }
+    public IEnumerable<Content>? PlaybookContentList { get; set; }
 		public Account? LoginUserInfo { get; set; }
 	}
 }
