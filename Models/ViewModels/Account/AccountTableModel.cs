acbf95fe213a492a703a6b6b042d26f531c3cac9
AlphaGamingArcade <155131996+AlphaGamingArcade@users.noreply.github.com>
2025-04-21 09:41:43 +0800

    chores: clean up partial tables

--- Models/ViewModels/Account/AccountTableModel.cs
+++ Models/ViewModels/Account/AccountTableModel.cs
@@ -4,9 +4,8 @@ namespace Winterra.Models.ViewModels
 {
     public class AccountTableModel
     {
-        public string Title { get; set; } = "Account";
-        public string MenuIn { get; set; } = "";
-        public string SeeAllAction { get; set; } = "Index";
-        public List<Account> Items { get; set; } = new();
+        public string? Title { get; set; }
+        public string? MenuIn { get; set; }
+        public IEnumerable<Account>? Items { get; set; }
     }
 }
--- Models/ViewModels/Content/ContentTableModel.cs
+++ Models/ViewModels/Content/ContentTableModel.cs
@@ -4,9 +4,8 @@ namespace Winterra.Models.ViewModels
 {
     public class ContentTableModel
     {
-        public string Title { get; set; } = "Content";
-        public string MenuIn { get; set; } = "";
-        public string SeeAllAction { get; set; } = "Index";
-        public List<Content> Items { get; set; } = new();
+        public string? Title { get; set; }
+        public string? MenuIn { get; set; }
+        public List<Content>? Items { get; set; }
     }
-}     
+}
